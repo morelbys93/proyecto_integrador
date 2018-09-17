f7543a00f58d654f06b917f3ec4604e8dcd6ab4b
morelbys93 <42612392+morelbys93@users.noreply.github.com>
2018-09-17 13:52:08 -0300

    Proyecto

---
+++ Queen/home.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- My CSS -->
+		<link rel="stylesheet" href="css/style.css">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+		<link rel="icon" href="images/ico.jpg" type="image/x-icon">
+
+		<title>QueenCandy| HOME</title>
+	</head>
+	<body>
+
+	<div class="container">
+
+		<!-- cabecera -->
+		<header class="main-header">
+			<img src="images/queen_candy.png" alt="logotipo" class="logo">
+			<!-- Menu -->
+			<nav class="main-nav">
+				<ul>
+					<li><a href="#">Registrate</a></li>
+					<li><a href="#">Ingresar</a></li>
+					<li><a href="#">#########</a></li>
+					<li><a href="#">#########</a></li>
+					<li><a href="#">##########</a></li>
+
+				</ul>
+			</nav>
+		</header>
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/uno.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/dos.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/tres.jpg" alt="Third slide">
+    </div>
+		<div class="carousel-item">
+      <img class="d-block w-100" src="images/cuatro.jpg" alt="Fourth slide">
+    </div>
+		<div class="carousel-item">
+      <img class="d-block w-100" src="images/cinco.jpg" alt="Fifth slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+		<!-- footer -->
+		<footer class="main-footer">
+			<section class="redes">
+        <ul>
+          <li>  <a href="https://twitter.com/MorelbysAnais"><i class="fab fa-twitter"></i></a>
+								<a href="https://www.facebook.com/morelbys.rodriguez.1"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/morerodrigg/"><i class="fab fa-instagram"></i></a>
+
+          </li>
+        </ul>
+      </section>
+		</footer>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+</html>
