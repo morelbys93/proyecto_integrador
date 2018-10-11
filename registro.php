68577cfb8c8548454daae2ba6862c4a32b3421b4
Morelbys Rodriguez <more@localhost.localdomain>
2018-10-11 12:35:50 -0300

    colocando ico y titulo

--- registro.php
+++ registro.php
@@ -19,15 +19,18 @@ if ($_POST) {
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title> QueenCandy | REGISTRO </title>
 		<!-- My CSS -->
+
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Font Awesome -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styleregistro.css">
+		<!-- Favicon -->
 		<link rel="icon" href="images/ico.jpg" type="image/x-icon">
+		<!-- titulo -->
+		<title>QueenCandy| REGISTRO</title>
 	</head>
 	<body>
 	<div class="container">
@@ -37,7 +40,7 @@ if ($_POST) {
       <img src="images/queen_candy.png" alt="logotipo" class="logo">
 		  </header>
     </a>
-
+		<!-- REGISTRO -->
 		<div class="container">
 			<div class="row">
 				<form role="form" action="" method="post" enctype="multipart/form-data">
