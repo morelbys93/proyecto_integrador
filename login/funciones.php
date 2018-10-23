aaacb3fb89e62a8c81255b8f7fb3de2f09b2a072
dylanddd <dorfmandylan@gmail.com>
2018-10-23 18:59:15 -0300

    login

---
+++ login/funciones.php
@@ -0,0 +1,13 @@
+<?php
+function loginUsuario($datoslogin){
+  $archivo= file_get_contents("datos.json");
+  $datos= json_decode($archivo,true);
+
+  for ($i=0; $i < count($datos["usuarios"]); $i++) {
+    if ($datos["usuarios"][$i]["username"]==$datosLogin["username"]) {
+      if (password_verify($datosLogin["contrasena"],$datos["usuarios"][$i]["contrasena"])) {
+        session_start();
+        $_SESSION['user']= $datos["usuarios"][$i]["username"];
+        header("location:perfil.php");
+}
+ ?>
---
+++ login/login.php
@@ -0,0 +1,24 @@
+<?php
+require 'funciones.php';
+if ($_POST) {
+  loginUsuario($_POST);
+}
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Queen Candy | login</title>
+</head>
+<body>
+  <div class="container">
+    <div class="form">
+      <input type="text" placeholder="usuario">
+      <input type="text" placeholder="password">
+      <button>login</button>
+      <p class="registro">No se encuentra registrado?<a href=""> Crear una cuenta</a></p>
+    </div>
+  </div>
+</body>
+</html>
