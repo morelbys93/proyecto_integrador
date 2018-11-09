bbc8689ee977d2ccf21942b444ab5613fbbc4be4
Sebasds7 <sebastian.ds7@gmail.com>
2018-11-09 10:52:00 -0300

    a ver ahora

--- pregfrec.php
+++ pregfrec.php
@@ -30,7 +30,7 @@
     <div class="container">
     <ul>
       <li class="pregu"><p>Cuales son los metodos de pago?</p></li>
-      <li  class="resp"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p></li>
+      <li  class="resp"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. UDSADASDAt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p></li>
       <li class="pregu"><p>A que zonas llega el envio?</p></li>
       <li  class="resp"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p></li>
       <li class="pregu"><p>Cuanto tarda el envio?</p></li>
